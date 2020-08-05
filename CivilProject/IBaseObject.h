bf412a47559167a99e164c31c18be95994a20280
wandererOdmolyboh <gresh135@gmail.com>
2020-08-05 09:51:24 +0300

    Add Unit Test

--- CivilProject/IBaseObject.h
+++ CivilProject/IBaseObject.h
@@ -14,13 +14,12 @@ public:
 
 public:
 	virtual Rect* boundingBox() = 0; // insert to another interface
-	virtual void set(std::vector<double>& tmp) = 0;
-	virtual void DrawObject() const = 0;
-	virtual bool isValid() const = 0;
+  virtual void DrawObject() const = 0;
+  virtual bool isValid() const = 0;
 	//
-
-	virtual void save(IWriter*&) = 0;
-	virtual void load(IReader*&) = 0;
+	//virtual void save(IWriter*&) = 0;
+	//virtual void load(IReader*&) = 0;
+  virtual void set(std::vector<double>& tmp) = 0;
 protected:
 	void setName(std::string) ;
 	std::string getName() const;
---
+++ UnitTest/UnitTest.cpp
@@ -0,0 +1,48 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest
+{
+	TEST_CLASS(UnitTest)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+  TEST_CLASS(UCopyConstruct)
+  {
+  public:
+
+    TEST_METHOD(TestMethod1)
+    {
+    }
+  };
+  TEST_CLASS(USetTest)
+  {
+  public:
+
+    TEST_METHOD(TestMethod1)
+    {
+    }
+  };
+  TEST_CLASS(UDrawTest)
+  {
+  public:
+
+    TEST_METHOD(TestMethod1)
+    {
+    }
+  };
+  TEST_CLASS(USetTest)
+  {
+  public:
+
+    TEST_METHOD(TestMethod1)
+    {
+    }
+  };
+}
