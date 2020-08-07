c34bf85d579b7c9d63cf627e30321f6669d4194c
wandererOdmolyboh <gresh135@gmail.com>
2020-08-07 09:54:47 +0300

    first unit test ArcImpl

--- UnitTest/UnitTest.cpp
+++ UnitTest/UnitTest.cpp
@@ -6,14 +6,39 @@ using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 namespace UnitTest
 {
-	TEST_CLASS(UnitTest)
+	TEST_CLASS(ImplArcTest)
 	{
 	public:
-		
-		TEST_METHOD(TestMethod1)
+		TEST_METHOD(BaseFunctional)
 		{
-      ArcImpl *p = new ArcImpl();
-      Assert::AreEqual(1, 1);
+      try// todo correct processing bad alloc or replace to notrow
+
+        /*
+         ArcImpl *p = new(nothrow) ArcImpl();
+         if (p == NULL)
+        */
+      {
+        ArcImpl *p = new ArcImpl();
+        p->setAngleFirst(0.);
+        p->setAngleSecond(160.);
+        p->setRadiuse(0.4);
+        p->setCenter(Point2d(5., 5.));
+
+        Assert::AreEqual(0., p->getAngleFirst());
+        Assert::AreEqual(160., p->getAngleSecond());
+
+        Assert::AreEqual(0.4, p->getRadiuse());
+
+        Assert::AreEqual(5., p->getCenter().x());
+        Assert::AreEqual(5., p->getCenter().y());
+        delete p;
+      }
+      catch (std::bad_alloc&t)
+      {
+        std::cout << t.what() << std::endl;
+      }
+
+
 		}
 
 	};
