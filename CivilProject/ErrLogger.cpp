cd739d00b9a6a12201a7a137568ca619c1dc378b
wandererOdmolyboh <gresh135@gmail.com>
2020-08-10 11:00:58 +0300

    Add Logger error

--- CivilProject/ErrLogger.cpp
+++ CivilProject/ErrLogger.cpp
@@ -2,18 +2,22 @@
 
 void ErrLogger::file_error(int e)
 {
+
 }
 
 void ErrLogger::data_error(int e)
 {
+
 }
 
 void ErrLogger::error(int e)
 {
+
 }
 
 ErrLogger::~ErrLogger()
 {
+
 }
 
 ErrLogger & ErrLogger::operator=(const ErrLogger & rhs)
